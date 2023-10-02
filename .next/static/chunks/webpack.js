50a104eac299279f971acd5f46867a8b7230e543
mustafagencturk <34949440+mustafagencturk@users.noreply.github.com>
2023-10-02 13:51:13 +0300

    versiyon v1.07
    clear cache data and revision

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "84648ceadf45d338"; }
+/******/ 		__webpack_require__.h = function() { return "93021da755c426bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
